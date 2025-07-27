fc11a9e2e538a39062f3daf6e065747976a4830f
rawr <rawr@kali>
2025-07-27 02:23:36 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import './App.css'
 import Page from './components/Page'
+import { HashRouter } from 'react-router-dom';
 function App() {
 
   return (
-    <>
+    <HashRouter>
       <div className='flex flex-col m-auto min-h-dvh md:bg-Pale-blue md:items-center md:justify-center '>
         <Page></Page>
       </div>
-
-    </>
+    </HashRouter>
+    
   )
 }
 
